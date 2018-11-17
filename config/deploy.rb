76f22257ac832286bc60f53486c7e4f7e1fd4d00
Iratu <admin@rori-dl.com>
2018-11-17 16:36:59 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, 'iqdbs'
 set :repo_url, 'git://github.com/Iratu/iqdbs.git'
 set :rbenv_type, :user
-set :rbenv_ruby, "2.3.1"
+set :rbenv_ruby, "2.4.0"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids')
 set :linked_files, fetch(:linked_files, []).push(".env")
